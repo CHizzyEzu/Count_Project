a3cb1db542f342c63730c68f937847e74009ab76
CHizzyEzu <155769930+CHizzyEzu@users.noreply.github.com>
2024-04-03 17:09:48 -0400

    Reformatted the code to include more conditons for input validation and restructured loops

--- Counter.cpp
+++ Counter.cpp
@@ -1,42 +1,54 @@
 // Counter Assignment 
 // Junior Ezuma-Ngwu Code Project
 
+/* This program prompts that the user to enter two integers, one between 0 and -30,
+and the other between 15 and 30. It then prints all the integers between those ranges, display 10 numbers
+per line*/
+
 #include <iostream>
+#include <limits>
 using namespace std;
 
 int main()
 {
-// Asking for users input of first number
 
-    double Input1;
-
-    do 
-    {
-        cout << "Enter a number between 0 and -30:  ";
-        cin >> Input1;
-
-    } while (Input1 <= -30 && Input1 >= 0);
-
-
-// Asking for users input of second number
-
-    double Input2;
+    // Asking for users input of first number
+    signed int input1;
 
     do 
     {
-        cout << "Enter another number between 15 and 30:  ";
-        cin >> Input2;
-
-
-    } while (Input2 > 30 && Input2 < 15); 
-
+        cout << "Enter a number between 0 and -30: ";
+        if(!(cin >> input1) || (input1 < -30 || input1 > 0))
+        {
+            cout << "Invalid input. Please enter a number between 0 and -30. \n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
     
+    // Postest Input loop (This displays first)
+    } while (input1 < -30 || input1 > 0);
+    
+    // Asking for users input of second number
+        int input2;
 
-// Print a new number with a break after 10 numbers 
+    do
+    {
+        cout << "Enter another number between 15 and 30: ";
+        if(!(cin >> input2) ||(input2 < 15 || input2 > 30))
+        {
+            cout << "Invalid input. Please enter a number between 15 and 30: \n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
 
+    // Second postest loop
+    } while (input2 < 15 || input2 > 30); 
+
+    // Initialize increment variable for the pretest loop
     int count = 0;
 
-    for (double c = Input1; c <= Input2; c++) 
+    // Pretest loop that creates a new line after an input is incremented 10 times
+    for (int c = input1; c <= input2; c++) 
     {
         cout << c << " ";
         count++;
@@ -46,12 +58,6 @@ int main()
             cout << "\n";
             count = 0;
         }
-
-        
     }
-
-
-    return 0;
-
-
+    return 0; // code ends
 }
